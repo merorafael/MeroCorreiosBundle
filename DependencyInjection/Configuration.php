8ba6aa93cd72ba054559aab18f3c4a38550c595f
Rafael Mello <merorafael@gmail.com>
2017-06-03 16:31:15 -0300

    Bugfix on extension namespace

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Mero\Bundle\BaseBundle\DependencyInjection;
+namespace Mero\Bundle\CorreiosBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
